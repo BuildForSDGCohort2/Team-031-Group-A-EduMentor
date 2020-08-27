d0ef7cc3bcfeed560f91a5df2a50daa04e474bb7
LAWRENCE David <David.LAWRENCE@bourbon-online.com>
2020-08-27 17:48:05 +0100

    Update for codacy review
    Signed-off-by: LAWRENCE David <David.LAWRENCE@bourbon-online.com>

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
 if (process.env.NODE_ENV === "production") {
+  // eslint-disable-next-line import/no-unresolved
   require("./dist");
 } else {
   require("nodemon")({ script: "dev.js" });
