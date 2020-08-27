79bfc008097a74da11d73e5fda75b2c453d80df9
LAWRENCE David <David.LAWRENCE@bourbon-online.com>
2020-08-27 12:06:21 +0100

    Code review
    Signed-off-by: LAWRENCE David <David.LAWRENCE@bourbon-online.com>

--- src/config/dependencyInjector.js
+++ src/config/dependencyInjector.js
@@ -1,10 +1,11 @@
 import dependencyManager from "../lib/dependencyManager";
 import config from "./index";
+const mongoose = require("mongoose");
 
 dependencyManager.register("logger", () => {
   require("../lib/logger").create(config.logging.development);
 });
 
 dependencyManager.register("mongoose", () => {
-  require("mongoose");
+  return mongoose;
 });
