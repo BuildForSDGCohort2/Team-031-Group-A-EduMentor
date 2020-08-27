f79d80ebcd3245b6685b1f39bb6fbff1aeb448bb
LAWRENCE David <David.LAWRENCE@bourbon-online.com>
2020-08-27 11:22:32 +0100

    Setup dependecy injection
    Signed-off-by: LAWRENCE David <David.LAWRENCE@bourbon-online.com>

--- src/config/dependencyInjector.js
+++ src/config/dependencyInjector.js
@@ -4,3 +4,7 @@ import config from "./index";
 dependencyManager.register("logger", () => {
   require("../lib/logger").create(config.logging.development);
 });
+
+dependencyManager.register("mongoose", () => {
+  require("mongoose");
+});
