64444b99dc004066d54bc9816e87932027e2878e
LAWRENCE David <David.LAWRENCE@bourbon-online.com>
2020-08-27 12:14:42 +0100

    Updated for codacy review
    Signed-off-by: LAWRENCE David <David.LAWRENCE@bourbon-online.com>

--- src/config/dependencyInjector.js
+++ src/config/dependencyInjector.js
@@ -3,9 +3,9 @@ import config from "./index";
 const mongoose = require("mongoose");
 
 dependencyManager.register("logger", () => {
-  require("../lib/logger").create(config.logging.development);
+    require("../lib/logger").create(config.logging.development);
 });
 
 dependencyManager.register("mongoose", () => {
-  return mongoose;
+    return mongoose;
 });
