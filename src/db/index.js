51e6065206bd3654dcb288e73a371f1fb2e38487
LAWRENCE David <David.LAWRENCE@bourbon-online.com>
2020-08-27 11:46:33 +0100

    Setup DB connection
    Signed-off-by: LAWRENCE David <David.LAWRENCE@bourbon-online.com>

---
+++ src/db/index.js
@@ -0,0 +1,34 @@
+import dependencyManager from "../lib/dependencyManager";
+import logger from "../lib/logger";
+
+class Database {
+  
+  constructor(username,password) {
+    this.mongoose = dependencyManager.get("mongoose");
+    this._connect(host,username,password);
+  }
+
+  async _connect(username,password) {
+    this.mongoose.Promise = global.Promise;
+    this.mongoose.connect(`mongodb+srv://${username}:${password}@edumentor.5kxd0.mongodb.net/<dbname>?retryWrites=true&w=majority`);
+    const {connection} = this.mongoose;
+    connection.on("connected", () =>
+      logger.info("Database Connection was Successful")
+    );
+    connection.on("error", (err) =>
+      logger.info(`Database Connection Failed ${err}`)
+    );
+    connection.on("disconnected", () =>
+      logger.info("Database Connection Disconnected")
+    );
+    process.on("SIGINT", () => {
+      connection.close();
+      logger.info(
+        "Database Connection closed due to NodeJs process termination"
+      );
+      process.exit(0);
+    });
+  }
+}
+
+module.exports = Database;
