8c7de98fdb0bffeba24fddc7943930805b96be1e
LAWRENCE David <David.LAWRENCE@bourbon-online.com>
2020-08-27 16:26:48 +0100

    Updated
    Signed-off-by: LAWRENCE David <David.LAWRENCE@bourbon-online.com>

--- src/db/index.js
+++ src/db/index.js
@@ -2,25 +2,18 @@ import dependencyManager from "../lib/dependencyManager";
 import logger from "../lib/logger";
 
 class Database {
-  
-  constructor(username,password) {
+  constructor(username, password) {
     this.mongoose = dependencyManager.get("mongoose");
-    this._connect(host,username,password);
+    this.connect(username, password);
   }
 
-  async _connect(username,password) {
+  async connect(username, password) {
     this.mongoose.Promise = global.Promise;
     this.mongoose.connect(`mongodb+srv://${username}:${password}@edumentor.5kxd0.mongodb.net/<dbname>?retryWrites=true&w=majority`);
-    const {connection} = this.mongoose;
-    connection.on("connected", () =>
-      logger.info("Database Connection was Successful")
-    );
-    connection.on("error", (err) =>
-      logger.info(`Database Connection Failed ${err}`)
-    );
-    connection.on("disconnected", () =>
-      logger.info("Database Connection Disconnected")
-    );
+    const { connection } = this.mongoose;
+    connection.on("connected", () => logger.info("Databases Connection was Successful"));
+    connection.on("error", (err) => logger.info(`Database Connection Failed ${err}`));
+    connection.on("disconnected", () => logger.info("Database Connection Disconnected"));
     process.on("SIGINT", () => {
       connection.close();
       logger.info(
