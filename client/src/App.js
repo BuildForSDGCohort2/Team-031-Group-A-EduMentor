24005e02bb2e606b4b81b6e543c8bb76ad8dee89
Festus Ebin <ebinfestus@gmail.com>
2020-09-04 18:56:50 +0100

    new changes

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Welcome to EduMentor
+        </h2>
+        <p>Online College/University Mentorship Platform</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
