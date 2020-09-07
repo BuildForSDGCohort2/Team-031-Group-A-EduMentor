8cb48278ca266fffd8e437b78be84e7d1f17b908
LAWRENCE David <David.LAWRENCE@bourbon-online.com>
2020-09-07 15:15:28 +0100

    Modified server startup process
    Signed-off-by: LAWRENCE David <David.LAWRENCE@bourbon-online.com>

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
-import express from "express";
-import path from "path";
-import loader from "./api/loaders";
+import app from "./api";
 import config from "./api/config";
-import Database from "./api/config/dbConnection";
 import logger from "./api/config/winstonlog";
 
-const app = express();
+/**
+ *
+ * @param {Object} server
+ * @desc - Sets up handlers for different exit events,
+ * to ensure server closes properly and for testing control
+ */
+function setupExitHandlers(server) {
+  // https://stackoverflow.com/a/14032965/971592
+  async function exitHandler(options = {}) {
+    await server
+      .close()
+      .then(() => {
+        logger.info("Server closed succesfully");
+        if (options.exit) {
+          process.exit(1);
+        }
+      })
+      .catch((error) => logger.warn(`Error closing server: ${error.stack}`));
+  }
+  // do something when app is closing
+  process.on("exit", exitHandler);
+  // catches ctrl + c event
+  process.on("SIGINT", exitHandler.bind(null, { exit: true }));
+  // catches "kill pid" e.g nodemon restarts
+  process.on("SIGUSR1", exitHandler.bind(null, { exit: true }));
+  process.on("SIGUSR2", exitHandler.bind(null, { exit: true }));
+  // catches uncaught exceptions
+  process.on("uncaughtException", exitHandler.bind(null, { exit: true }));
+}
 
-// Initialize database connection
-const db = new Database();
-db.connect(config.DbUrl);
+/**
+ *
+ * @param {Object} Configuration object
+ * @des - Starts the server on the provided port
+ * @returns - a Promise that resolves with the Http.Server object
+ */
+function startServer({ port = process.env.PORT } = {}) {
+  return new Promise((resolve) => {
+    // app.listen returns a server object
+    const server = app.listen(port || 3000, (event) => {
+      if (event) { logger.info(event); } else { logger.info(`listening on port: ${server.address().port}`); }
 
-// Initialize app with dependencies and error handlers
-loader.init(app);
+      // wrap .close method of server object with a promise
+      // first we save a binding of the original close(), piggy Backing 🤐
+      const originalClose = server.close.bind(server);
 
-// To use other UI routes
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./src/index.html"));
-});
-
-app.listen(config.port || 5000, (event) => {
-  if (event) { logger.info(event); } else { logger.info(`listening on port: ${config.port}`); }
-});
+      // then overwrite it to return a Promise
+      server.close = () => new Promise(() => {
+        originalClose();
+      });
+    });
+    // Setup handlers for different process termination events
+    setupExitHandlers(server);
+    resolve(server);
+  });
+}
+// Start the server
+startServer({ port: config.port })
+  .catch((error) => {
+    logger.warn(`Error starting server: ${error.stack}`);
+    process.exit(1);
+  });
